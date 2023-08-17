85f604fb32154c2a7757a62ece9c46bb4d991fd6
Hannah <hcour@gmail.com>
2023-08-17 06:58:31 -0400

    minor update

--- models.py
+++ models.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = 'blogly'
     id = db.Column(db.Integer, 
                    primary_key=True,
                    autoincrement=True)
